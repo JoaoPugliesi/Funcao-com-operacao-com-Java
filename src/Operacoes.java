6188d829156c54da1bc037cd4a58f8daf8b61609
João <joaopaulo.allex@gmail.com>
2022-03-21 13:41:13 -0300

    Projeto acadêmico

---
+++ src/Operacoes.java
@@ -0,0 +1,17 @@
+
+public class Operacoes {
+	
+	public static String contador(int i, int f) {
+		String s = "";
+		for(int c = i; c <= f; c++) {
+			s+= c + " ";
+		}
+		return s;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/TesteFuncao02.java
@@ -0,0 +1,10 @@
+
+public class TesteFuncao02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vai começar a contagem");
+		System.out.println(Operacoes.contador(1,5));
+	}
+
+}
